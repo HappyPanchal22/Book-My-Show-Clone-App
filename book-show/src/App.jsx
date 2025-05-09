6e9fed94fc13c876e68d8b66ff741136947849a6
Happy Panchal <panchalhappy22@gmail.com>
2025-05-09 17:20:36 +0530

    SetUp Done

---
+++ book-show/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import React from 'react'
+import ReactDOM from 'react';
+import {Routes, Route} from "react-router-dom";
+import './App.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+      <div>Hello Welcome to my world.</div>
+      /*<Routes>
+        <Route path ="/" element={<HomePage/>}/>
+        <Route path ="/movie/:id" element={<MoviePage/>}/>
+        <Route path = "/plays" element={<Plays/>}/>
+      </Routes>*/
+      
+  )
+}
+
+export default App
